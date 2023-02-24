c54fe14564a61725c945bf678f520e8c804ede8b
DEIVIS ANDRES HERRERA JULIO <dherrerj@contratista.epm.co>
2023-02-24 17:11:21 -0500

    Sección 11: Routing y navegación - Angular (105)

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routing, AppRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { PaginaErrorComponent } from './components/pagina-error/pagina-error.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +20,21 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     SliderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginaComponent,
+    PaginaErrorComponent,
+    PeliculasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Routing
   ],
-  providers: [],
+  providers: [
+    AppRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend-angular/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule, Route } from '@angular/router';
+
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { PaginaErrorComponent } from './components/pagina-error/pagina-error.component';
+
+const appRoutes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'peliculas', component: PeliculasComponent },
+  { path: 'pagina', component: PaginaComponent },
+  { path: '**', component: PaginaErrorComponent }
+];
+
+export const AppRoutingProviders: any[] = [];
+export const Routing: ModuleWithProviders<Route> = RouterModule.forRoot(appRoutes);
