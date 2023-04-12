1d6d09ef4dd88e12562d3bfcc6c0ba1d1ea7f274
DEIVIS ANDRES HERRERA JULIO <dherrerj@contratista.epm.co>
2023-04-12 15:00:18 -0500

    Sección 36: Proyecto completo con Vue JS desde cero y paso a paso (243)

---
+++ frontend-vue/src/components/modules/administraciones/articles/ArticlesComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="articles" v-if="articles && articles.length > 0">
+    <article class="article-item" v-for="article in articles" v-bind:key="article._id">
+      <div class="image-wrap">
+        <img v-if="article.image" :src="url + '/article/imagen/' + article.image" :alt="article.title" />
+        <img v-if="!article.image" src="./../../../../assets/images/default.jpg" alt="Image Default" />
+      </div>
+      <h2>{{ article.title }}</h2>
+      <span class="date">
+        {{ article.date }}
+      </span>
+      <RouterLink to="/blog/articulo">Leer más</RouterLink>
+      <div class="clearfix"></div>
+    </article>
+  </div>
+</template>
+<script>
+import Global from './../../../app-config/Default';
+
+export default {
+  name: 'ArticlesComponent',
+  props: {
+    articles: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      url: Global.url
+    };
+  }
+}
+</script>
--- frontend-vue/src/components/modules/administraciones/blog/BlogComponent.vue
+++ frontend-vue/src/components/modules/administraciones/blog/BlogComponent.vue
@@ -4,6 +4,7 @@
     <div class="center">
       <section id="content">
         <h2 class="subheader">Blog</h2>
+        <ArticlesComponent :articles="articles"></ArticlesComponent>
       </section>
       <SidebarComponent></SidebarComponent>
       <div class="clearfix"></div>
@@ -11,14 +12,49 @@
   </div>
 </template>
 <script>
+import Axios from 'axios';
+import Global from './../../../app-config/Default';
+
 import SliderComponent from './../../../layout/slider/SliderComponent.vue';
 import SidebarComponent from './../../../layout/sidebar/SidebarComponent.vue';
+import ArticlesComponent from './../articles/ArticlesComponent.vue'
 
 export default {
   name: 'BlogComponent',
   components: {
     SliderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ArticlesComponent
+  },
+  mounted() {
+    this.getArticles();
+  },
+  data() {
+    return {
+      url: Global.url,
+      status: null,
+      articles: []
+    };
+  },
+  methods: {
+    getArticles() {
+      let lastArticles = this.props.lastArticles;
+      let searchedArticle = this.props.searchedArticle;
+      let urlArticles = this.url + '/articles';
+
+      if (lastArticles && !searchedArticle) {
+        urlArticles += '/' + lastArticles;
+      } else if (!lastArticles && searchedArticle) {
+        urlArticles = this.url + '/article/search/' + searchedArticle;
+      }
+
+      Axios.get(urlArticles).then(response => {
+        this.articles = response.data.articles;
+      }).catch(function (error) {
+        console.log(error);
+        this.articles = [];
+      });
+    }
   }
 }
 </script>
